70d8a4454fe93aff939858d1a526533dfa3ee09a
PenwellCodes <164396512+PenwellCodes@users.noreply.github.com>
2025-02-24 14:23:39 +0200

    updated api key

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 const Page = () => {
-  return <Redirect href="/(screen)/home" />;
+  return <Redirect href="/(tabs)/home" />;
 };
 
-export default Page;
+export default Page
