8eee23006c06e0fd3174de0be66a3b6ab7edb47b
Culture <111436414+CodehubNerd@users.noreply.github.com>
2025-02-25 10:43:59 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 const Page = () => {
-  return <Redirect href="/(screens)/home" />;
+  return <Redirect href="/(tabs)/home" />;
 };
 
 export default Page;
